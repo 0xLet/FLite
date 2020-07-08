715756d25a8b2cddaa111f095ca7d0940265a74e
Zach Eriksen <zeriksen@clientresourcesinc.com>
2020-07-08 16:41:03 -0500

    Added iOS 13 support to SQLiteKit

--- Package.swift
+++ Package.swift
@@ -19,7 +19,8 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0-beta.2"),
-        .package(url: "https://github.com/vapor/sqlite-nio.git", .branch("master"))
+        .package(url: "https://github.com/vapor/sqlite-nio.git", .branch("master")),
+        .package(url: "https://github.com/0xLeif/sqlite-kit", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -28,7 +29,8 @@ let package = Package(
             name: "FLite",
             dependencies: [
             .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
-            .product(name: "SQLiteNIO", package: "sqlite-nio")
+            .product(name: "SQLiteNIO", package: "sqlite-nio"),
+            .product(name: "SQLiteKit", package: "sqlite-kit")
         ]),
         .testTarget(
             name: "FLiteTests",
