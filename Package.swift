d5cc929370db0d9b214c3eb8d1b611a0c66c7732
Zach Eriksen <zeriksen@clientresourcesinc.com>
2020-07-08 16:44:33 -0500

    Added iOS 13 support to SQLite Driver

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0-beta.2"),
+        .package(url: "https://github.com/0xLeif/fluent-sqlite-driver.git", .branch("master")),
         .package(url: "https://github.com/vapor/sqlite-nio.git", .branch("master")),
         .package(url: "https://github.com/0xLeif/sqlite-kit", .branch("master"))
     ],
