50ac2275f59ecd17f8d9d9cb095d6e92d1fbe40e
Zach Eriksen <zeriksen@clientresourcesinc.com>
2020-07-08 16:31:19 -0500

    Require iOS 13+

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "FLite",
     platforms: [
-        .iOS("11.0"),
+        .iOS("13.0"),
         .macOS("10.15")
     ],
     products: [
